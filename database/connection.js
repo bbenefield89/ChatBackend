61fa2c131b62f446e821c2325ff021f4bb64d7cc
bbenefield89 <bsquared18@gmail.com>
2018-09-30 20:51:26 -0400

    UPDATE CONNECTION TO USE PostgreSQL

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,10 @@
 const Sequelize = require('sequelize')
 
-const { DBNAME, DBUSER, DBPW } = process.env
+const { DBNAME, DBUSER, DBPW, DBHOST } = process.env
 
 const sequelize = new Sequelize(DBNAME, DBUSER, DBPW, {
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  operatorsAliases: false
+  host: DBHOST,
+  dialect: 'postgresql'
 })
 
 module.exports = sequelize
