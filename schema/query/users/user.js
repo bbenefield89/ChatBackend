6451afbb819bf8a6d123469b3d91ff32a8d9b835
bbenefield89 <bsquared18@gmail.com>
2018-10-01 01:53:21 -0400

    CREATE SCHEMA & NEW UserType QUERY
    - schema/index.js
    - schema/query/users/user.js

---
+++ schema/query/users/user.js
@@ -0,0 +1,18 @@
+const graphql = require('graphql')
+
+const Users = require('../../../database/models/users')
+
+const {
+  GraphQLID, GraphQLObjectType, GraphQLString
+} = graphql
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLID },
+    username: { type: GraphQLString },
+    password: { type: GraphQLString }
+  })
+})
+
+module.exports = UserType
