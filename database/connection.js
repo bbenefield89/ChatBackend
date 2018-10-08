f8cc4d7d69b3a85d9a4b6b6aa4b16af241d34e9f
bbenefield89 <bsquared18@gmail.com>
2018-10-08 15:51:38 -0400

    NOW USES A URI TO CONNECT TO DB
    - database/connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,15 +2,6 @@ require('dotenv').config()
 
 import { Sequelize } from 'sequelize'
 
-let sequelize
-
-if (process.env.NODE_ENV === 'development') {
-  const { DBNAME, DBUSER, DBPW, DBHOST, DBPORT, DATABASE } = process.env
-  sequelize = new Sequelize(
-    `${ DATABASE }://${ DBUSER }:${ DBPW }@${ DBHOST }:${ DBPORT }/${ DBNAME }`
-  )
-}
-else
-  sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 export { sequelize }
