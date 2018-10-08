7609c709fd3c5a2b79f543c9e8ecb2caa7fa32de
bbenefield89 <bsquared18@gmail.com>
2018-10-08 15:40:13 -0400

    A LOT OF NEW CONFIGURATION TO GET THE APP RUNNING CORRECTLY
    - app.js
      - brought in 'cors' to allow for ajax calls from the client
      - created a 'Message' model to store messages in the DB
      - open cors up for the 'ws' websocket server (look into using the 'cors' package)
      - documentation

--- app.js
+++ app.js
@@ -2,32 +2,62 @@ require('dotenv').config()
 
 import express                from 'express'
 import { ApolloServer, gql }  from 'apollo-server-express'
+import cors                   from 'cors'
 import { execute, subscribe } from 'graphql'
 import { createServer }       from 'http'
 import { SubscriptionServer } from 'subscriptions-transport-ws'
 
 import { sequelize as db }  from './database/connection'
-import { typeDefs, schema } from './graphql/schema'
+import Message              from './database/models/Message'
+import { schema, typeDefs } from './graphql/schema'
 import { resolvers }        from './graphql/resolvers'
 
-const PORT         = process.env.PORT || 3001;
-const app          = express();
-const ws           = createServer(app);
+const PORT = process.env.PORT || 3001
+const app  = express()
+
+/**
+ * TODO: use an env variable to decide the correct
+ *       'Access-Control-Allow-Origin' for this, 'ws', and for 'app.use(cors())'
+ * 
+ * the callback function pretty much open cors up for everyone
+ */
+const ws = createServer(app, (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:${ PORT }`)
+  res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+})
+
 const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+app.use(cors())
 
 // allows express to serve the '/graphql' endpoint/graphiql-playground
 apolloServer.applyMiddleware({ app })
 
+/**
+ * this portion is pretty special
+ * in a normal express app we would typically see 'app.listen(...)' but here we
+ * are using the 'ws' variable, which points to the 'createServer(app)'
+ * function. Effectively what we have done here is created two separate
+ * servers where one, our express app (app), is being served over HTTP at port
+ * 3001, 'http://localhost:3001', and we are also serving up another server (ws),
+ * thanks to 'SubscriptionServer.create(...)', on the same port, '3001', but over
+ * a different protocol, 'ws' or WebSocket. We can connect our subscriptions from
+ * the client to this server at 'ws://localhost:3001/graphql'
+ */
 ws.listen(PORT, async () => {
   console.log(`WS listening at http://localhost:${ PORT }`)
 
-  await db.authenticate()
-  // try {
-  //   console.log('\n\n=====\nDATABASE CONNECTED\n=====\n\n')
-  // }
-  // catch (err) {
-  //   console.log(err)
-  // }
+  try {
+    await db.authenticate()
+    console.log('\n\n=====\nDATABASE CONNECTED\n=====\n\n')
+
+    await Message.sync()
+  }
+  catch (err) {
+    console.log(err)
+  }
 });
 
 SubscriptionServer.create(
