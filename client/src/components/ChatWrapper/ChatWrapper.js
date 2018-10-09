cbc0734c5031ea7de9098337f27b9f0c8906ae77
bbenefield89 <bsquared18@gmail.com>
2018-10-08 22:01:39 -0400

    YET ANOTHER METHOD TO DECIDE IF A USER IS LOGGED IN
    - client/src/components/ChatWrapper/ChatWrapper.js
      - should also be worked on (confusing)

--- client/src/components/ChatWrapper/ChatWrapper.js
+++ client/src/components/ChatWrapper/ChatWrapper.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import ChatForm from './ChatForm'
 import ChatMessagesList from './ChatMessagesList'
 
-const ChatWrapper = ({ auth, username }) => {
-  return (
-    <React.Fragment>
-      <ChatMessagesList
-        auth={ auth }
-      />
+class ChatWrapper extends Component {
+  state = {}
+  
+  verifyLogin = async () => {
+    const authOisAuthenticated = () => (
+      new Promise((res, rej) => {
+        res(this.props.auth.isAuthenticated())
+      }
+    ))
 
-      <ChatForm
-        auth={ auth }
-        username={ username }
-      />
-    </React.Fragment>
-  )
+    const data = await authOisAuthenticated()
+
+    if (data)
+      this.props.isAuthenticated()
+    else
+      this.props.history.push('/')
+  }
+  
+  componentDidMount() {
+    // this.verifyLogin()
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <ChatMessagesList
+          auth={ this.props.auth }
+        />
+  
+        <ChatForm
+          auth={ this.props.auth }
+          username={ this.props.username }
+        />
+      </React.Fragment>
+    )
+  }
 }
  
 export default ChatWrapper
