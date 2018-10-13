a786282ab4f223cae9f8290a71993751ba53a8bd
bbenefield89 <bsquared18@gmail.com>
2018-10-13 12:58:18 -0400

    REMOVE UNUSED CODE
    - graphql/resolvers.js
    - graphql/schema.js

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,50 +3,14 @@ import { PubSub } from 'graphql-subscriptions'
 import Message from '../database/models/Message'
 
 const pubsub       = new PubSub();
-const HELLO_ADDED  = 'helloAdded'
-const USER_CREATED = 'userCreated'
 const MESSAGE_CREATED = 'messageCreated'
-
-const hellos = [{
-  id: 1,
-  message: 'Hello 1'
-}]
-let helloId = hellos.length
-
-const users = [{ id: 1, username: 'user1', password: 'pass1', email: 'email1' }]
-let userID = users.length
 
 const resolvers = {
   Query: {
-    hellos: () => hellos,
-    
-    hello: (root, { id }) => {
-      return hellos.find(hello => hello.id == id)
-    },
-
-    messages: async () => Message.findAll(),
-
-    users: () => users,
-    user: (root, { id }) => users.find(user => user.id == id)
+    messages: async () => Message.findAll()
   },
 
   Mutation: {
-    newHello: (root, args) => {
-      const newHello = { id: String(++helloId), message: args.message }
-
-      hellos.push(newHello)
-
-      pubsub.publish(HELLO_ADDED, { helloAdded: newHello })
-      return newHello
-    },
-
-    createUser: (root, { username, password, email }) => {
-      const newUser = { id: ++userID, username, password, email }
-      users.push(newUser)
-      pubsub.publish(USER_CREATED, { userCreated: newUser })
-      return newUser
-    },
-
     createMessage: async (root, { username, message }) => {
       const newMessage = await Message.create({ username, message })
       pubsub.publish(MESSAGE_CREATED, { newMessage })
@@ -55,14 +19,6 @@ const resolvers = {
   },
 
   Subscription: {
-    helloAdded: {
-      subscribe: () => pubsub.asyncIterator(HELLO_ADDED)
-    },
-
-    userCreated: {
-      subscribe: () => pubsub.asyncIterator(USER_CREATED)
-    },
-
     newMessage: {
       subscribe: () => pubsub.asyncIterator(MESSAGE_CREATED)
     }
--- graphql/schema.js
+++ graphql/schema.js
@@ -3,15 +3,10 @@ import {  makeExecutableSchema } from 'graphql-tools'
 import { resolvers } from './resolvers'
 
 const typeDefs = `
-  type Hello {
-    id: ID!
-    message: String
-  }
-
   type User {
     id: ID!
-    username: String!
-    email: String!
+    nickname: String!
+    picture: String!
   }
 
   type Message {
@@ -21,22 +16,17 @@ const typeDefs = `
   }
   
   type Query {
-    hellos: [Hello]!
-    hello(id: ID!): Hello
-    users: [User]!
+    users: [ User ]!
     user(id: ID!): User
     messages: [ Message ]!
     message: Message
   }
 
   type Mutation {
-    newHello(message: String!): Hello
-    createUser(username: String!, password: String!, email: String!): User
     createMessage(username: String!, message: String!): Message
   }
 
   type Subscription {
-      helloAdded: Hello
       userCreated: User
       newMessage: Message
   }
