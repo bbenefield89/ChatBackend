ab2908bae98897ae95876d6b4c1246503d4b386c
bbenefield89 <bsquared18@gmail.com>
2018-10-13 12:57:38 -0400

    STYLE NAV COMPONENT
    - client/src/components/Nav/Nav.js

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,31 +1,106 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import styled from 'styled-components'
 
-const Nav = ({ auth, isLoggedIn, username }) => {
+import limbochat from '../static/limbochat.png'
+
+const NavbarStyled = styled.nav`
+  background: #647795;
+  display: grid;
+  grid-template: auto / 100%
+  grid-row-gap: 20px;
+  padding: 20px 0;
+
+  @media (min-width: 524px) {
+    grid-template: auto / repeat(2, 1fr);
+  }
+`
+
+const NavbarBrandStyled = styled.a`
+`
+
+const ImgStyled = styled.img`
+  width: 200px;
+`
+
+const NavStyled = styled.ul`
+  list-style: none;
+  margin: 0 auto 0 auto;
+  padding: 0;
+  width: 80%;
+
+  @media (min-width: 524px) {
+    display: flex;
+    align-items: center;
+  }
+`
+
+const NavItemStyled = styled.li`
+  width: 100%;
+`
+
+const ButtonStyled = styled.button`
+  background: #393D3F;
+  border: 2px solid #393D3F;
+  color: #fafafa;
+  font-size: 1.6rem;
+  letter-spacing: 4px;
+  padding: 5px 0;
+  max-width: 220px;
+  width: 100%;
+`
+
+class Navigation extends PureComponent { /*({ auth, isLoggedIn, username }) => { */
   /**
    * TODO: this is the exact same logic for the 'login/signup' button in
    *       <Home /> make sure to make own component to reduce recreating same
    *       methods
    */
-  const signup = () => {
-    auth.login()
+
+  state = {
+    isOpen: true
   }
   
-  return (
-    <nav>
-      <p>{ username }</p>
+  signup = () => {
+    this.props.auth.login()
+  }
 
-      {
-        isLoggedIn
-        ?
-          <button onClick={ auth.logout }>Log out</button>
-        :
-          <React.Fragment>
-            <button onClick={ signup }>Sign Up</button>
-            <button onClick={ signup }>Log In</button>
-          </React.Fragment>
-      }
-    </nav>
-  )
+  toggleNavbar = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  
+  render() {
+    return (
+      <NavbarStyled className='nav_wrapper'>
+        <NavbarBrandStyled className='nav_wrapper__company_image'>
+          <ImgStyled
+            src={ limbochat }
+          />
+        </NavbarBrandStyled>
+
+        <NavStyled className='nav_wrapper__links_list'>
+          <NavItemStyled className='nav_wrapper__links_list__item'>
+          {
+            this.props.isLoggedIn
+            ?
+              <ButtonStyled
+                className='nav_wrapper__links_list__item__logout_btn'
+                onClick={ this.props.auth.logout }>
+                  Log out
+              </ButtonStyled>
+            :
+              <ButtonStyled
+                className='nav_wrapper__links_list__item__login_btn'
+                onClick={ this.signup }>
+                  Enter Limbo
+              </ButtonStyled>
+          }
+          </NavItemStyled>
+        </NavStyled>
+      </NavbarStyled>
+    )
+  }
 }
  
-export default Nav
+export default Navigation
