8f32e5a31b33436070bf93c91a92eb2944084fe0
bbenefield89 <bsquared18@gmail.com>
2018-10-13 12:56:29 -0400

    SMALL JS FILE TO HOLD INCENTIVES DATA
    - client/src/components/Home/incentiveData.js
      - this file holds information for each 'incentive' component on the home page

---
+++ client/src/components/Home/incentiveData.js
@@ -0,0 +1,24 @@
+import online from '../static/online.svg'
+import world from '../static/world.svg'
+import browsers from '../static/browsers.svg'
+
+const incentiveData = [
+  {
+    incentiveHeaderText: 'Sign in anonymously or create a new profile',
+    incentiveImg: online
+  },
+  {
+    /**
+     * look into grabbing the current amount of users in the chat and display
+     * that number in this entry
+     */
+    incentiveHeaderText: 'Instantly connect with who ever is logged in from all over the world',
+    incentiveImg: world
+  },
+  {
+    incentiveHeaderText: 'Probably works in most major browsers',
+    incentiveImg: browsers
+  },
+]
+
+export default incentiveData
